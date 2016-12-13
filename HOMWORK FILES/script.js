b1c551599b58a81c4f0f7c99166cca3e54c4c675
ehmed-kino <ehmedaff@gmail.com>
2016-12-13 11:54:42 +0000

    fixing live srearch output

--- HOMWORK FILES/script.js
+++ HOMWORK FILES/script.js
@@ -4,12 +4,13 @@ function load() {
   var map = document.createElement("img");
   map.setAttribute('src', 'images/map.png');
   document.getElementById('pic').appendChild(map);
+
+  // create buttons
 }
 // Ajax API
 function request() {
-
   var http = new XMLHttpRequest;
-  http.open('GET', "https://code-your-future.github.io/api-demo/area/index.json");
+  http.open('GET', "https://code-your-future.github.io/api-demo/index.json");
 
   http.onreadystatechange = function() {
 
@@ -20,16 +21,25 @@ function request() {
     // set a var for search object
     var searchBar = document.getElementById('search');
 
-    // looping through the json links
+    // looping through json file 
     for(var i=0; i<myData.data.length; i++) {
-      // check if the value exsiest
-      if(myData.data[i].toLowerCase().search(searchBar.value.toLowerCase()) !== -1) {
-        document.getElementById('pp').innerHTML = myData.data[i];
+      for(key in myData.data[i]) {
+
+        // check if the value exsiest
+        if(myData.data[i][key].toLowerCase().search(searchBar.value.toLowerCase()) !== -1) {
+          // how to print all current Objects
+          demo(myData.data[i].toSource());
+          // check the outputs
+          console.log(myData.data[i][key]);
+        }
+
+        }
       }
-      // check the value of the search bar
-      console.log(search.value);
-    }
     }
   }
   http.send();
+}
+// this is for demostration
+function demo(data) {
+    document.getElementById('pp').innerHTML += data + "<br>";
 }
