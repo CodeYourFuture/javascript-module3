7a93b81e35d040982a78c5cdc742a6eeb63e4253
Md-Almasri <mohieddin.almasri@hotmail.com>
2016-12-15 14:25:02 +0000

    adding style

--- script.js
+++ script.js
@@ -1,41 +1,27 @@
-var jsonString;
 var divContainer = document.getElementById("container");
 var first = true;
-
-var http = new XMLHttpRequest();
-/*******
-// Specify the Method and the URL we want to access
-http.open('GET', 'https://code-your-future.github.io/api-demo/area/index.json');
-
-// Function to be called as the request happens
-http.onreadystatechange = function() {
-	// readyState === 4 means the request has finished (http://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp)
-	// status === 200  means the request was OK
-	if (this.readyState === 4 && this.status === 200) {
-		jsonString = (this.responseText); // The body returned by the server (as a string)
-		logThis();
-	}
-}
-http.send(); // Make the request
-*******/
-
 function getData(url) {
+	var http = new XMLHttpRequest();
+	// Specify the Method and the URL we want to access
 	http.open('GET', url);
+	// Function to be called as the request happens
 	http.onreadystatechange = function() {
+		// readyState === 4 means the request has finished (http://www.w3schools.com/xml/ajax_xmlhttprequest_response.asp)
+		// status === 200  means the request was OK
 		if (this.readyState === 4 && this.status === 200) {
-			jsonString = (this.responseText); // The body returned by the server (as a string)
 			if (first) {
-				logThis();
+				logThis(this.responseText);// The body returned by the server (as a string)
 				first = false;
 			}else {
-				logThat();
+				logThat(this.responseText);
 			};
 		};
 	};
-	http.send();
+	http.send(); // Make the request
 };
-function logThis() {
+function logThis(jsonString) {
 	var myObject = JSON.parse(jsonString);
+	console.log(myObject);
 	for (var key in myObject) {
 		for (var i in myObject[key]) {
 			createBtn([myObject[key][i]]);
@@ -43,25 +29,37 @@ function logThis() {
 	};
 	addingEvents();
 };
-function logThat() {
+function logThat(jsonString) {
+	createDiv();
 	document.getElementById('info').innerHTML = '';
 	var myObject = JSON.parse(jsonString);
+	console.log(myObject);
 	for (var i in myObject['data']) {
 		for (var j in myObject['data'][i]) {
 			document.getElementById('info').innerHTML += ([j] + ':  ' + myObject['data'][i][j] + '</br>');
 		};
 		document.getElementById('info').innerHTML += '</br>' + '--------------' + '</br>';
 	};
-}
+};
 function createBtn(textNode) {
 	var btn = document.createElement("BUTTON");
 	var text = document.createTextNode(textNode);
 	btn.appendChild(text);
+	var attr = document.createAttribute("class")
+	attr.value = "btnArea";
+	btn.setAttributeNode(attr);
 	divContainer.appendChild(btn);
 	// var x = document.getElementById("container");
 	// for (var i = 0; i < 1; i++) {
 	// 	x.innerHTML += '<button type="button" class="btn" onclick="logThis()">' + textNode + '</button>';
 	// }
+};
+function createDiv() {
+	var div = document.createElement("DIV");
+	var attr = document.createAttribute("id")
+	attr.value = "info";
+	div.setAttributeNode(attr);
+	divContainer.appendChild(div);
 };
 function addingEvents() {
 	var btn = document.getElementsByTagName('button');
@@ -72,9 +70,6 @@ function addingEvents() {
 function info() {
 	var t = this.innerHTML;
 	var url = 'https://code-your-future.github.io/api-demo/area/' + t + '/index.json';
-	console.log(t , url);
 	getData('https://code-your-future.github.io/api-demo/area/' + t + '/index.json');
-	
-
-}
+};
 getData('https://code-your-future.github.io/api-demo/area/index.json');
