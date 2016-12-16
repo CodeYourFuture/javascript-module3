d477981220176eafd31fcbd5d25a5211730ec474
ehmed-kino <ehmedaff@gmail.com>
2016-12-16 11:43:31 +0000

    change the whole js content

--- HOMWORK FILES/script.js
+++ HOMWORK FILES/script.js
@@ -1,45 +1,48 @@
-// when loading the page
 function load() {
-  // creatingthe map
-  var map = document.createElement("img");
-  map.setAttribute('src', 'images/map.png');
-  document.getElementById('pic').appendChild(map);
-
-  // create buttons
+  createMap();
+  createButtons();
 }
-// Ajax API
-function request() {
+
+function createMap() {
+  var map = document.createElement('img');
+  map.setAttribute('src', 'images/map.png');
+  map.setAttribute('width', '100%');
+  document.getElementById('pic').appendChild(map);
+}
+function createButtons() {
+
   var http = new XMLHttpRequest;
-  http.open('GET', "https://code-your-future.github.io/api-demo/index.json");
-
+  http.open('GET', "https://code-your-future.github.io/api-demo/area/index.json");
   http.onreadystatechange = function() {
-
-  if(this.readyState === 4 && this.status === 200 ) {
-    //parsing json file
-    var myData = JSON.parse(http.responseText);
-
-    // set a var for search object
-    var searchBar = document.getElementById('search');
-
-    // looping through json file 
-    for(var i=0; i<myData.data.length; i++) {
-      for(key in myData.data[i]) {
-
-        // check if the value exsiest
-        if(myData.data[i][key].toLowerCase().search(searchBar.value.toLowerCase()) !== -1) {
-          // how to print all current Objects
-          demo(myData.data[i].toSource());
-          // check the outputs
-          console.log(myData.data[i][key]);
-        }
-
-        }
+    if(this.readyState === 4 && this.status === 200) {
+      var myData = JSON.parse(http.responseText);
+      var buttons = '';
+      for(var i=0; i<myData.data.length; i++) {
+          buttons = '<button id="btn-' + myData.data[i] + '" type=\"button\" onClick=createSubButtons("' + myData.data[i] + '")>' + myData.data[i] + '</button>';
+        document.getElementById('pop-ups').innerHTML += buttons;
       }
     }
   }
   http.send();
 }
-// this is for demostration
-function demo(data) {
-    document.getElementById('pp').innerHTML += data + "<br>";
+
+function createSubButtons(areaName) {
+  var http = new XMLHttpRequest;
+  http.open('GET', "https://code-your-future.github.io/api-demo/area/" + areaName + "/index.json");
+  http.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200) {
+      document.getElementById('sub-pop-ups').innerHTML = "" ;
+      var myData = JSON.parse(http.responseText);
+      var buttons = '';
+      for(var i=0; i<myData.data.length; i++) {
+          buttons = '<button id="btn-' + myData.data[i].organisation + '" type=\"button\" onClick=createSubButtons("' + myData.data[i].organisation + '")>' + myData.data[i].organisation + '</button>';
+          document.getElementById('sub-pop-ups').innerHTML += buttons;
+          console.log(' : ' + myData.data[i].organisation);
+
+        console.log("----------------------------------------");
+
+      }
+    }
+  }
+  http.send();
 }
