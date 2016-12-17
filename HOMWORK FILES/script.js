21e7c3d4ac1794bd99e538c5ce8655e4b07a72c3
ehmed-kino <ehmedaff@gmail.com>
2016-12-17 23:48:02 +0000

    my whole work was wrong

--- HOMWORK FILES/script.js
+++ HOMWORK FILES/script.js
@@ -34,6 +34,51 @@ function createButtons() {
   }
   http.send();
 }
+
+
+function createSubButtons(areaName) {
+  var http = new XMLHttpRequest;
+  http.open('GET', "https://code-your-future.github.io/api-demo/area/" + areaName + "/index.json");
+  http.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200) {
+      document.getElementById('sub-pop-ups').innerHTML = "" ;
+      var myData = JSON.parse(http.responseText);
+      var buttons = '';
+      for(var i=0; i<myData.data.length; i++) {
+          buttons = '<button id="btn-' + myData.data[i].organisation + '" type=\"button\" onClick=createBox("' + myData.data[i].borough + '")>' + myData.data[i].organisation + '</button>';
+          document.getElementById('sub-pop-ups').innerHTML += buttons;
+          console.log(' : ' + myData.data[i].organisation);
+
+        console.log("----------------------------------------");
+
+      }
+    }
+  }
+  http.send();
+}
+// I have a problem here
+function createBox(boroughName) {
+  var newBoroughName = boroughName.replace(' ' , '-');
+  var http = new XMLHttpRequest;
+  http.open('GET', "https://code-your-future.github.io/api-demo/borough/" + newBoroughName + "/index.json");
+  http.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200) {
+      var myData = JSON.parse(http.responseText);
+
+      document.getElementById('pp').innerHTML = "";
+      // OMG OMG OMG this is the most stuoied thing I've ever been through
+      for(var i=0; i<myData.data.length; i++) {
+        document.getElementById('pp').innerHTML += "<p><strong>organisation : </strong>" + myData.data[i].organisation + "<br>"
+                                                  + "<p><strong>Website:  </strong>" + myData.data[i].website; + '<br>'
+                                                  + "<p><strong>Email : </strong>" + myData.data[i].email + '<br>'
+                                                  + "<p><strong>days : </strong>" + myData.data[i].days;
+      }
+    }
+  }
+  http.send();
+
+}
+
 function reqSearch() {
 
   var http = new XMLHttpRequest;
@@ -51,28 +96,6 @@ function reqSearch() {
           createSubButtons(myData.data[i]);
         } // if ends
       }// for ends
-    }
-  }
-  http.send();
-}
-
-
-function createSubButtons(areaName) {
-  var http = new XMLHttpRequest;
-  http.open('GET', "https://code-your-future.github.io/api-demo/area/" + areaName + "/index.json");
-  http.onreadystatechange = function() {
-    if(this.readyState === 4 && this.status === 200) {
-      document.getElementById('sub-pop-ups').innerHTML = "" ;
-      var myData = JSON.parse(http.responseText);
-      var buttons = '';
-      for(var i=0; i<myData.data.length; i++) {
-          buttons = '<button id="btn-' + myData.data[i].organisation + '" type=\"button\" onClick=createSubButtons("' + myData.data[i].organisation + '")>' + myData.data[i].organisation + '</button>';
-          document.getElementById('sub-pop-ups').innerHTML += buttons;
-          console.log(' : ' + myData.data[i].organisation);
-
-        console.log("----------------------------------------");
-
-      }
     }
   }
   http.send();
