1e7868738f4ec309ebc4bf22316181b6ddac00f6
Angela <Baby007mix@gmail.com>
2017-02-09 21:34:44 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,72 @@
-	
-	var immigration = document.getElementById("container");
-	var btn = document.getElementById("btn");
-	var link="https://code-your-future.github.io/api-demo/area/";
-	var currentOption=false;
+//load the overlay
+window.addEventListener("load",()=>{
+	const overlay = document.getElementById('overlay');
+	overlay.style.display = 'none';
+});
 
-	function requestData(dataSource){
+const request = new XMLHttpRequest();
+const serviceByAreas = document.getElementById('displayAreas');
 
-		var ourRequest = new XMLHttpRequest();
-		
-		ourRequest.open('GET', dataSource, true);
-		ourRequest.onload = function () {
-
-			//if (request.status >= 200 && request.status < 400) {
-		var ourData = JSON.parse(ourRequest.responseText);
-		//if the operation is true run renderhtml()
-		if(!currentOption){
-			renderHTML(ourData);
+request.open('GET', 'https://code-your-future.github.io/api-demo/area/index.json', true);
+request.onload = element=>{
+	if (request.status>=200 && request.status<400){
+		let data = JSON.parse(request.responseText);
+		for (let i = 0; i < data.data.length; i++) {
+			//console.log(data.data);
+			const element = document.createElement('div');
+			element.style.margin = "2px 1px";
+			element.style.width = "170px";
+			element.innerHTML = '<button id = "btnArea" onclick = "areaData(\''+data.data[i]+'\')">'+data.data[i]+'</button>';
+			serviceByAreas.appendChild(element);
 		}
-		else{ 
-
-			for (var i = 0; i<ourData.data.length; i++) {
-				
-				//for (var j = 0; j<ourData.data[i].length; j++) {
-					for(key in ourData.data[i]){
-						//console.log(ourData.data[i][key]);
-						//document.write(ourData.data[i][key])
-						var keyInfo = (ourData.data[i]);
-						//I want to harvest the index item ie website, area, telephone..etc
-						//console.log(ourData.data[i]);
-						var displayInfo = (ourData.data[i][key]);
-						console.log(displayInfo);
-				}
+	}	else{ 
+		console.log('Oh Oh! There seems to be a problem');
+		//request.onerror();
 		}
+};
+request.send();
 
-		}
-			
+//Display Error Message
+request.onerror = ()=> {
+	document.write("&#x21bb Something's Went Wrong! Please try Again! :(");
+}
 
-		};
-		ourRequest.send();
-		
-	}
-	
-	function renderHTML(data) {
-		var htmlString = "";
-		for (i = 0; i < data.data.length; i++) {
-			//htmlString+= "<a href='" + link + data.data[i] + "/index.json'>" + data.data[i]+"</a><br>"; //Change it to button
-			htmlString+= "<a href='#' alt='" + link + data.data[i] + "/index.json'>" + data.data[i]+"</a><br>"; 
-			//console.log (htmlString);
-		}
-		immigration.innerHTML=htmlString;
-		//container.insertAdjacentHTML('beforeend', htmlString);
-	}
-
-	btn.addEventListener("click", function() {
-		requestData(link+"/index.json");
-	})
-
-	document.getElementById('container').addEventListener('click',function(q){
-			if(q.target.tagName==='A'){
-				//console.log(q.target.getAttribute('alt'));
-				currentOption=true;
-				requestData(q.target.getAttribute('alt'));
-
+const areaData = id=> {
+	const newRequest = new XMLHttpRequest();
+	newRequest.open('GET','https://code-your-future.github.io/api-demo/area/'+ id +'/index.json', true);
+	newRequest.onload = ()=> {
+		serviceDetails.innerHTML = "";
+		if (newRequest.status === 200) {
+			let data = JSON.parse(newRequest.responseText);
+			for (let j = 0; j < data.data.length; j++) {
+				serviceProviders(data.data[j]);
 			}
+		}
+		else{
+			//request.onerror();
+			console.log('Oh Oh! There seems to be a problem');
+		}
+	};
+newRequest.send();
+};
+const serviceDetails = document.getElementById('displayServices');
+const serviceProviders = service=> {
+	let element = document.createElement('box');
+	serviceDetails.innerHTML+= "<h2>" + service.organisation +"</h2>";
+	let listing = document.createElement('div');
+	listing.style.wordWrap = "break-word";
+	let listingData = "<p><strong>Telephone: </strong>" + service.tel + "</p>";
+	listingData += "<p><strong>Website: </strong>" + service.website + "</p>";
+	listingData += "<p><strong>Email: </strong>" + service["email\r"] + "</p>";
+	listingData += "<p><strong>Opening Hours: </strong>" + service.day + "</p>";
+	listingData += "<p><strong>Help With: </strong>" + service.type + "</p>";
+	listingData += "<p><strong>Services Provided: </strong>" + service.services + "</p>";
+	listingData += "<p><strong>Clients: </strong>" + service.clients+ "</p>";
+	listingData += "<p><strong>Areas Covered: </strong>"+ service.area+"</p>";
+	listingData += "<p><strong>Borough: </strong>"+service.borough+"</p>";
+	listingData += "<p><strong>How We Operate: </strong>"+service.process+"</p>";
 
-	},false)
-	
-
-
-	/* I am not sure how to do the rest. I can't understand how to get the other url 
-		to work so I can assign a button to call the JSON data. I am going to continue to 
-		try to understand it*/
+	listing.innerHTML += listingData;
+	element.appendChild(listing);
+	serviceDetails.appendChild(element); 
+}
